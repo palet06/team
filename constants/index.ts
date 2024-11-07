e07b5180512f558d1ee8a2100896addcc6e27005
Murat Hayaloglu <murath31@hotmail.com>
2024-11-07 22:14:47 +0300

    Left Sidebar, Navlinks with extra responsiveness

---
+++ constants/index.ts
@@ -0,0 +1,17 @@
+export const sidebarLinks = [
+  {
+    imgURL: "/github.svg",
+    label: "Anasayfa",
+    route: "/",
+  },
+  {
+    imgURL: "/github.svg",
+    label: "Deneme",
+    route: "/deneme",
+  },
+  {
+    imgURL: "/github.svg",
+    label: "Profil",
+    route: "/profile",
+  },
+];
